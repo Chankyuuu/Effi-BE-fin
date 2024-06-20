7d2a5bbc398c40edbc5591e1f4793d641b10ed29
chankyuu <shin4438@gmail.com>
2024-06-20 17:06:02 +0900

    Fix : cors 수정

--- src/main/java/com/example/effi/config/SecurityConfig.java
+++ src/main/java/com/example/effi/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                config.setAllowedOrigins(Collections.singletonList("http://effi-fe-s3.s3-website.ap-northeast-2.amazonaws.com"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
