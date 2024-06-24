cb34f574b12dd66515fc759d35ec35e04c9ba930
chankyuu <shin4438@gmail.com>
2024-06-24 11:20:17 +0900

    Fix : 권한 sudo 실행

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,11 @@ PROJECT_ROOT="/home/ec2-user/app/deploy"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 # 디렉토리 생성 및 권한 설정
-mkdir -p $PROJECT_ROOT
-chown ec2-user:ec2-user $PROJECT_ROOT
-chmod 755 $PROJECT_ROOT
+sudo mkdir -p $PROJECT_ROOT
+sudo chown ec2-user:ec2-user $PROJECT_ROOT
+sudo chmod 755 $PROJECT_ROOT
 
 # 로그 파일 생성 및 권한 설정
-touch $DEPLOY_LOG
-chown ec2-user:ec2-user $DEPLOY_LOG
-chmod 666 $DEPLOY_LOG
+sudo touch $DEPLOY_LOG
+sudo chown ec2-user:ec2-user $DEPLOY_LOG
+sudo chmod 666 $DEPLOY_LOG
